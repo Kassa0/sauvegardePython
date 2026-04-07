534a3b145715151e15dd26c8cc290a7a0b5dcd67
Kassa0 <hprotteau@gmail.com>
2026-04-07 09:19:11 +0200

    1.1Gestion De Clés Sécurisée

--- index.py
+++ index.py
@@ -1,16 +1,21 @@
 from cryptography.fernet import Fernet
+import os
 
 
 def sauvegarder_cle(cle, nom_fichier):
-    # TODO: Écrire la clé en binaire
-    # TODO: os.chmod() → 0o600
-    return
+    # On ouvre en 'wb' (Write Binary) car la clé est un objet bytes
+    with open(nom_fichier, 'wb') as fichier_cle:
+        fichier_cle.write(cle)
+
+    # On change les permissions : 0o600 = seul le propriétaire peut lire/écrire
+    os.chmod(nom_fichier, 0o600)
 
 
 def charger_cle(nom_fichier):
-    # TODO: Lire le fichier en binaire
-    # TODO: Retourner la clé (bytes)
-    return
+    # On ouvre en 'rb' (Read Binary)
+    with open(nom_fichier, 'rb') as fichier_cle:
+        # On lit et on retourne directement le contenu (bytes)
+        return fichier_cle.read()
 
 
 cle = Fernet.generate_key()
