e4d6298040dac36958340688d0d476e2ef7b0210
Kassa0 <hprotteau@gmail.com>
2026-04-07 09:50:34 +0200

    B1 . TP1

--- tp1.py
+++ tp1.py
@@ -1,3 +1,6 @@
+from cryptography.fernet import Fernet
+
+
 def lire_fichier_binaire(chemin):
     """Lit un fichier en mode binaire"""
     with open(chemin, 'wb') as f:
@@ -9,3 +12,18 @@ def obtenir_taille_fichier(chemin):
     """Retourne la taille en octets"""
     import os
     return os.path.getsize(chemin)
+
+
+def chiffrer_message(message, cle):
+    """Chiffre un message avec la clé fournie"""
+    chiffre = Fernet(cle)       # ??? quel objet?
+    message_bytes = message.encode()  # ??? convertir en bytes
+    ciphertext = chiffre.encrypt(message_bytes)  # ??? méthode?
+    return ciphertext
+
+
+def dechiffrer_message(ciphertext, cle):
+    """Déchiffre un ciphertext"""
+    chiffre = Fernet(cle)
+    plaintext = chiffre.decrypt(ciphertext)  # ??? méthode?
+    return plaintext.decode()        # ??? convertir en string
